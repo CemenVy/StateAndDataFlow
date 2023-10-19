c6d5cd6ff5fdb649bcd2ef93c35a73b34a1dfbc1
Семен Выдрин <pw5hv7ngcv@privaterelay.appleid.com>
2023-10-19 13:57:29 +0400

    Added class StorageManager

--- StorageManager.swift
+++ StorageManager.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+final class StorageManager: ObservableObject {
+    static let shared = StorageManager()
+    
+    private let userDefaults = UserDefaults.standard
+    private let userKey = "user"
+    
+    private init() {}
+    
+    func fetchUser() -> User? {
+        guard let data = userDefaults.data(forKey: userKey) else { return nil }
+        do {
+            let user = try JSONDecoder().decode(User.self, from: data)
+            print(user)
+            return user
+        } catch {
+            print(error.localizedDescription)
+        }
+        return nil
+    }
+    
+    func save(user: User) {
+        guard let data = try? JSONEncoder().encode(user) else { return }
+        userDefaults.set(data, forKey: userKey)
+    }
+    
+    func deleteUser() {
+        userDefaults.removeObject(forKey: userKey)
+    }
+    
+}
