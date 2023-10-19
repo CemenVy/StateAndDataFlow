76115c71f304f8f76e54894e9521b80b7ae9cd23
Семен Выдрин <pw5hv7ngcv@privaterelay.appleid.com>
2023-10-19 13:56:13 +0400

    Added user Model

---
+++ StateAndDataFlow/Models/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  StateAndDataFlow
+//
+//  Created by Семен Выдрин on 19.10.2023.
+//
+
+import Foundation
+
+struct User: Codable {
+    var name: String
+    var isLoggedIn: Bool
+}
---
+++ StorageManager.swift
@@ -0,0 +1,8 @@
+//
+//  StorageManager.swift
+//  StateAndDataFlow
+//
+//  Created by Семен Выдрин on 19.10.2023.
+//
+
+import Foundation
